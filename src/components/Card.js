0dab0d0320a9c0f355d42eddcd3a4a9fed02baa0
Frid Matilda <matilda.x.frid@scania.com>
2023-12-28 16:46:52 +0100

    fixed drag

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,7 +3,7 @@ import { useDrag } from 'react-dnd';
 
 export const Card = ({ pickedBox, currentQuestionData }) => {
   const ref = useRef(null);
-  const [, drag, preview] = useDrag({
+  const [{ isDragging }, drag, preview] = useDrag({
     type: 'card',
     item: { pickedBox, currentQuestionData },
     collect: (monitor) => ({
@@ -11,12 +11,14 @@ export const Card = ({ pickedBox, currentQuestionData }) => {
     })
   });
 
+  const cardClassName = `card ${isDragging ? 'dragging' : ''}`;
+
   // Connect drag source and preview
   drag(ref);
   preview(ref);
 
   return (
-    <div className="card" ref={ref}>
+    <div className={cardClassName} ref={ref}>
       <h2>{currentQuestionData.word}</h2>
       <img src={currentQuestionData.picture} alt={currentQuestionData.word} />
     </div>
