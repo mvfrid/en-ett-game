844c3d30791d610daef8ce6e0b905ae6b31f98ae
Frid Matilda <matilda.x.frid@scania.com>
2024-01-08 20:44:43 +0100

    test

--- src/components/Game/CardPreview.js
+++ src/components/Game/CardPreview.js
@@ -6,8 +6,23 @@ export const CardPreview = ({ data }) => {
     return null;
   }
 
+  // Determine if the word is short, medium, or long.
+  const isShortWord = data.currentQuestionData.word.length < 5;
+  const isLongWord = data.currentQuestionData.word.length > 8;
+
+  let wordLengthClass = '';
+  if (isShortWord) {
+    wordLengthClass = 'shortWord';
+  } else if (isLongWord) {
+    wordLengthClass = 'longWord';
+  } else {
+    wordLengthClass = '';
+  }
+
+  const cardClassName = `card-preview ${wordLengthClass}`;
+
   return (
-    <div className="card-preview">
+    <div className={cardClassName}>
       <div className="card-text-box">
         <h2>{data.currentQuestionData.word}</h2>
         <p>({data.currentQuestionData.translation})</p>
